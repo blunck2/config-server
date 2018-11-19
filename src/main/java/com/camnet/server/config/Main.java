8fe73ed802d2fbb2468c3c27ec6705e40fbcf837
Christopher Blunck <chris@thebluncks.com>
2018-11-19 00:41:37 -0500

    added

---
+++ src/main/java/com/camnet/server/config/Main.java
@@ -0,0 +1,13 @@
+package com.camnet.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Main {
+  public static void main(String[] arguments) {
+    SpringApplication.run(Main.class, arguments);
+  }
+}
